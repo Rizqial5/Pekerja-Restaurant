b8b5b470a18c9c200df2e8b0bd576f26eadd43dc
Rizqial5 <bagusriski1@gmail.com>
2024-07-25 15:53:43 +0700

    LAstt

--- Assets/Script/Core/DayTimer.cs
+++ Assets/Script/Core/DayTimer.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SocialPlatforms;
 using UnityEngine.Events;
 
 namespace TestPR.Core
